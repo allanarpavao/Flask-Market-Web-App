1a4be185a23fe9ce137f8fe1b477c0eacf045917
allanapavao <allanarodrigues.p@gmail.com>
2026-04-24 13:36:00 -0300

    feat: add market page

--- market.py
+++ market.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route('/market'):
+@app.route('/market')
 def market_page():
-    return render_template('market.html')
+    items = [
+        {'id': 1, 'name': 'Tanzanite', 'barcode': 893212299897, 'price':500},
+        {'id': 2, 'name': 'Ametist', 'barcode': 123985473165, 'price': 900},
+        {'id': 3, 'name': 'Periodot', 'barcode': 231985128446, 'price': 150}
+    ]
+    return render_template('market.html', items=items)
